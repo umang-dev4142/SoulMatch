f1ea1986d43f9632292a5b748517bdb59c46b9f9
Umang Tiwari <officialumang6284@gmail.com>
2026-07-23 21:15:31 +0530

    Fix ASGI initialization order

--- soulmatch_app/asgi.py
+++ soulmatch_app/asgi.py
@@ -8,16 +8,19 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soulmatch_app.settings")
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soulmatch_app.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns)
     ),
