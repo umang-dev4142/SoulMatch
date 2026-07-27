ed65621110a772944958c7d05d78c9d42ad1dbe6
Umang Tiwari <officialumang6284@gmail.com>
2026-07-27 22:03:43 +0530

    Configure PostgreSQL and fix admin access

--- soulmatch_app/settings.py
+++ soulmatch_app/settings.py
@@ -13,9 +13,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
